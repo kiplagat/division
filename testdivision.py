820215910f20cbc02bbcf999ad834bb5fa12c525
Eric Kiplagat <ekiplagat@SamaDc.corp>
2017-10-10 11:45:19 +0300

    test division by zero

--- testdivision.py
+++ testdivision.py
@@ -3,5 +3,7 @@ from division import division
 class testdivision(unittest.TestCase):
      def test_answer_is_correct(self):
          self.assertEqual(division(8,4),2)
+     def test_result_is_not_infinite(self):
+	     self.assertEqual(TypeError,division(8,0),"infinite")
 if __name__ == "_main__":
  unittest.main()
